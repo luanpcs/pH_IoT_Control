266093a8011206d5e58f47f37f4cf723e591f86a
Luan <luancsiqueira@hotmail.com>
2023-09-10 23:26:40 -0300

    feat: :zap:

---
+++ MQTT_Module.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt
+
+MQTT_Client = mqtt.Client()
+MQTT_Broker = "broker.emqx.io"
+MQTT_Port = 1883
+MQTT_KeepAlive = 60
+MQTT_MessageDecode = 'utf-8'
+
+MQTT_SubTopic = "TCC_IoTpHControl/webPub"
+MQTT_PubTopic = "TCC_IoTpHControl/raspPub"
+
+def MQTT_Connect():
+    print("MQTT connecting...")
+    MQTT_Client.connect(MQTT_Broker, MQTT_Port, MQTT_KeepAlive)
+    print("MQTT connected!")
+
+def MQTT_Pub(msg):
+    print("<-", msg)
+    MQTT_Client.publish(MQTT_PubTopic, payload=msg)
+
+def MQTT_OnConnect(client, userdata, flags, rc):
+    MQTT_Client.subscribe(MQTT_SubTopic)
+    MQTT_Pub("Raspberry is connected")
+
+def MQTT_OnMessage(client, userdata, msg):
+    receivedMessage = msg.payload.decode(MQTT_MessageDecode)
+    print("->", receivedMessage)
+
+def MQTT_Init():
+    MQTT_Client.on_connect = MQTT_OnConnect
+    MQTT_Client.on_message = MQTT_OnMessage
+    MQTT_Client.connect(MQTT_Broker, MQTT_Port, MQTT_KeepAlive)
+    MQTT_Connect()
+    MQTT_Client.loop_forever()
+
+def mqtt_loop():
+    MQTT_Init()
+    MQTT_Client.loop_forever()
---
+++ pHControl.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+import threading
+import time
+
+from MQTT_Module import mqtt_loop, MQTT_Pub
+
+def main():
+    mqtt_thread = threading.Thread(target=mqtt_loop)
+    mqtt_thread.start()
+    time.sleep(2)
+    a = 0
+    while 1:
+        if a != 10:
+            MQTT_Pub(a)
+            a = a +1
+
+if __name__ == "__main__":
+    main()
