3826155211ed573c057345ccd42c34444b0b846a
Laura Dias <laura.c@gec.inatel.br>
2023-09-25 21:14:48 -0300

    feat: :sparkles: Complete alura example

--- Example/bytebank/lib/main.dart
+++ Example/bytebank/lib/main.dart
@@ -1,51 +1,25 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
-        appBar: AppBar(title: Text('Transferências')),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ListaTransferencias(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+          primary: Colors.green[900],
+          secondary: Colors.blueAccent[700],
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: TextButton.styleFrom(
+          backgroundColor: Colors.blueAccent[700],
+        )),
       ),
-    ));
-
-class ListaTransferencias extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia(100.0, 1000)),
-        ItemTransferencia(Transferencia(200.0, 1001)),
-        ItemTransferencia(Transferencia(300.0, 2000)),
-      ],
     );
-    throw UnimplementedError();
   }
-}
-
-class ItemTransferencia extends StatelessWidget
-{
-  final Transferencia _transferencia;
-
-  ItemTransferencia(this._transferencia);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text(_transferencia.valor.toString()),
-          subtitle: Text(_transferencia.numeroConta.toString()),
-    ));
-  }
-}
-
-class Transferencia
-{
-  final double valor;
-  final int numeroConta;
-
-  Transferencia(this.valor, this.numeroConta);
 }
