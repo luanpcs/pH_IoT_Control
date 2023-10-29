e1c5806760d74d39c02b1edf432d180bc4c3df81
Luan <luancsiqueira@hotmail.com>
2023-10-29 18:15:21 -0300

    refactor: Refactor mqtt handler

--- web-system/front/scripts/index-script.js
+++ web-system/front/scripts/index-script.js
@@ -5,13 +5,6 @@ export function hidePopups() {
     hideAlertasPopup();
     gerarGrafico()
     mqttInit()
-    savePH()
-    setInterval(sendId, 1000); // 1000 milissegundos = 1 segundo
-
-    /* 
-    login("user", "password")
-    registerUser("user", "password"); 
-    */
 }
 
 window.onload = hidePopups
--- web-system/front/scripts/mqtt.js
+++ web-system/front/scripts/mqtt.js
@@ -1,27 +1,62 @@
+const pubTopic = 'TCC_IoTpHControl/webPub';
+const subTopic = 'TCC_IoTpHControl/raspPub';
+const client = mqtt.connect('mqtt://broker.emqx.io:8083/mqtt');
+client.subscribe(subTopic);
+
 export function mqttInit() {
-    const client = mqtt.connect('mqtt://broker.emqx.io:8083/mqtt');
+    mqttSetCallBacks()
+    mqttScreenScan()
+}
 
-    client.on('connect', () => {
-        console.log('Conectado ao servidor MQTT');
-
-        // Publica uma mensagem em um tópico
-        const topic = 'TCC_IoTpHControl/webPub';
-        const message = 'Hello';
-        client.publish(topic, message);
-        console.log(`<- ${message.toString()}`);
-
+function mqttScreenScan() {
+    document.getElementById('mqttBtn-Aut').addEventListener('click', function () {
+        const payload = {msgType: 'modo',value: 'a'};
+        mqttSend(JSON.stringify(payload))
     });
 
-    client.on('message', (topic, message) => {
-        console.log(`-> ${message.toString()}`);
-    });
-
-    client.subscribe('TCC_IoTpHControl/raspPub');
-
-    client.on('error', (error) => {
-        console.error('Erro MQTT:', error);
+    document.getElementById('mqttBtn-Man').addEventListener('click', function () {
+        const payload = {msgType: 'modo', value: 'm'};
+        mqttSend(JSON.stringify(payload))
     });
 }
 
+function mqttSend(messageToSend) {
+    console.log(`<- ${messageToSend}`);
+    client.publish(pubTopic, messageToSend)
+}
 
+function mqttRec(payload) {
+    console.log(`-> ${payload}`);
 
+    const payloadJSON = JSON.parse(payload);
+
+    switch (payloadJSON.msgType) {
+        case 'phValue':
+            {
+                const valor = payloadJSON.value.toString()
+                document.getElementById('mqttData').textContent = valor;
+            }
+            break;
+
+        default:
+            {
+                console.log("Invalid message type")
+            }
+            break;
+    }
+
+}
+
+function mqttSetCallBacks() {
+    client.on('connect', () => {
+        mqttSend('Web system connected')
+    });
+
+    client.on('error', (error) => {
+        console.error('MQTT error:', error);
+    });
+
+    client.on('message', (topic, message) => {
+        mqttRec(message);
+    });
+}
