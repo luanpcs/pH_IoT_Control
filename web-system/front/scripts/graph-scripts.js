73555cc50e2e7442c7cd70b4120ff576005885a1
Luan <luancsiqueira@hotmail.com>
2023-11-23 23:37:35 -0300

    refactor init and wip graph

--- web-system/front/scripts/graph-scripts.js
+++ web-system/front/scripts/graph-scripts.js
@@ -1,39 +1,44 @@
+var canvas = document.getElementById("meuGrafico");
+var ctx = canvas.getContext("2d");
 
-function gerarGrafico() {
-    var canvas = document.getElementById("meuGrafico");
-    var ctx = canvas.getContext("2d");
+var data = {
+    labels: [],
+    datasets: [
+        {
+            label: "pH",
+            data: [],
+            borderColor: '#2F3B76',
+            backgroundColor: "transparent"
+        }
+    ]
+};
 
-    var data = {
-        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"],
-        datasets: [
-            {
-                label: "pH",
-                data: [7.2, 10.4, 3.7, 9.1, 12.9, 6.3, 1.5, 11.0, 8.6, 2.8, 5.2, 13.3, 4.9, 0.3, 14.0, 7.7, 3.1],
-                borderColor: '#2F3B76',
-                backgroundColor: "transparent"
+var meuGrafico = new Chart(ctx, {
+    type: "line",
+    data: data,
+    options: {
+        responsive: true,
+        elements: {
+            line: {
+                tension: 0.2, 
             }
-        ]
-    };
-
-    var meuGrafico = new Chart(ctx, {
-        type: "line",
-        data: data,
-        options: {
-            responsive: true,
-            elements: {
-                line: {
-                    tension: 0.4, // Valor que controla a suavidade da linha (ajuste conforme necessário)
-                }
-            },
-            scales: {
-                y: {
-                    ticks: {
-                        stepSize: 1, // Define o intervalo entre os rótulos no eixo Y
-                        min: 0, // Valor mínimo no eixo Y
-                        max: 14, // Valor máximo no eixo Y
-                    }
+        },
+        scales: {
+            y: {
+                min: 0, 
+                max: 15, 
+                ticks: {
+                    stepSize: 0.1,
                 }
             }
         }
-    });
+    }
+});
+
+
+function gerarGrafico(novoRotulo, novoDado) {
+    data.labels.push(novoRotulo);
+    data.datasets[0].data.push(novoDado);
+
+    meuGrafico.update();
 }
--- web-system/front/scripts/index-script.js
+++ web-system/front/scripts/index-script.js
@@ -6,7 +6,18 @@ import { mqttInit } from './mqtt.js';
 import { hideAutPopup } from './aut-script.js';
 import { hideManPopup } from './man-script.js';
 
-export function hidePopups() {
+export function systemInit() {
+    mqttInit()
+    screenInit()
+    // gerarMockDatas()
+
+
+    setInterval(function () {
+        //gerarGrafico(1, 2)
+    }, 1000);
+}
+
+function screenInit() {
     var setaAtualAut = document.getElementById('setaAtual');
     var setaAtualMan = document.getElementById('setaAtualMan');
     var textoAutAtual = document.getElementsByClassName('texto3');
@@ -25,32 +36,36 @@ export function hidePopups() {
         }
     }
 
-    hideRegistrosPopup();
-    hideAlertasPopup();
-    hideAutPopup()
-    hideManPopup()
-    hideReportPopup()
-    hidePreReportPopup()
-    gerarGrafico()
-    mqttInit()
+    hidePopups()
 
     const btnAut = document.getElementById('Btn-Aut')
     const btnMan = document.getElementById('Btn-Man')
 
     btnAut.classList.remove('modo-ativado')
     btnMan.classList.add('modo-ativado')
-    
-    // novoRegistro("Acidificante adicionado")
-    // novoRegistro("Alcanilizante adicionado")
-    // novoRegistro("Modo automático ativado")
-    // novoRegistro("Modo manual ativado")
-
-    // novoAlerta("pH inferior a 6.8")
-    // novoAlerta("pH superior a 7.5")
-    // novoAlerta("pH inferior a 6.1")
-    // novoAlerta("pH superior a 9.7")
-
 }
 
-window.onload = hidePopups
+function hidePopups() {
+    hideRegistrosPopup();
+    hideAlertasPopup();
+    hideAutPopup()
+    hideManPopup()
+    hideReportPopup()
+    hidePreReportPopup()
+}
+
+function gerarMockDatas()
+{
+    novoRegistro("Acidificante adicionado")
+    novoRegistro("Alcanilizante adicionado")
+    novoRegistro("Modo automático ativado")
+    novoRegistro("Modo manual ativado")
+
+    novoAlerta("pH inferior a 6.8")
+    novoAlerta("pH superior a 7.5")
+    novoAlerta("pH inferior a 6.1")
+    novoAlerta("pH superior a 9.7")
+}
+
+window.onload = systemInit
 
